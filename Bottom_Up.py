d57a6a7495f960c6f341f1235a91985a0aed4099
Atharsh S <97397128+Atharsh2001@users.noreply.github.com>
2022-08-26 15:40:58 +0530

    Update Bottom_Up.py

--- Bottom_Up.py
+++ Bottom_Up.py
@@ -1,17 +1,21 @@
 #Bottom_Up approach
+from itertools import combinations_with_replacement as com
 coins = [1,2,5]
-coins.sort(reverse=True)
 amount = 11
-arr = []
+arr = [0]*(amount+1)
 for i in range(1,amount+1):
-    sum = i
-    count=0
-    j=0
-    while sum>0:
-        if sum>=coins[j]:
-            sum-=coins[j]
-            count+=1
-        else:
-            j+=1
-    arr.append(count)
-print(arr[-1])
+    if i in coins:
+        arr[i]=1
+    else:
+        lis = [k for k in range(1,i)]
+        ans = []
+        for j in range(1,i+1):
+            t = list(com(lis,j))
+            for j in t:
+                if sum(j)==i:
+                    _=[]
+                    for i1 in j:
+                        _.append(arr[i1])
+                    ans.append(sum(_))
+        arr[i]=min(ans)
+print(arr)
